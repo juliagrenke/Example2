716e8ccf8c9316b4628d11fb7992e76851849345
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-27 16:17:50 +0500

    Task 36. DZ

---
+++ Seminar05_DZ_36/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, 
+//стоящих на нечётных позициях.
+//[3, 7, 23, 12] -> 19
+//[-4, -6, 89, 6] -> 0
+
+int[] GetRandomArray(int length)
+{
+    int[] array = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(-100, 100);
+    }
+    return array;
+}
+
+int SumElementsOddPos(int[] array)
+{
+    int sumOdd = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 > 0) sumOdd += 0;
+        else sumOdd += array[i];
+    }
+    return sumOdd;
+}
+
+Console.Write("Задайте количество элементов массива> ");
+int length1 = int.Parse(Console.ReadLine()!);
+int[] mass1 = GetRandomArray(length1);
+Console.WriteLine(string.Join(" ", mass1));
+Console.WriteLine(SumElementsOddPos(mass1));
+
+
