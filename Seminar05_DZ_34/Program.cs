7bd47bd719ade97ef6f6805f36d53eae1e0699a6
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-04-27 16:16:43 +0500

    Task 34. DZ

---
+++ Seminar05_DZ_34/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+//Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+int[] GetRandomArray(int length)
+{
+    int[] array = new int[length];
+    var rnd = new Random();
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+    }
+    return array;
+}
+
+int EvenNumberQuantity(int[] array)
+{
+    int n = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 > 0) n += 0;
+        else n += 1;
+    }
+    return n;
+}
+
+Console.Write("Задайте количество элементов массива> ");
+int length1 = int.Parse(Console.ReadLine()!);
+int[] mass1 = GetRandomArray(length1);
+Console.WriteLine(string.Join(" ", mass1));
+Console.WriteLine(EvenNumberQuantity(mass1));
+
+
