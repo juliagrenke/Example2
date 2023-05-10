49b05fe20a78f26803c8ca150e4aefa7096105cd
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-05-10 15:24:23 +0500

    ДЗ_Задача 41

--- Seminar06_DZ_41/Program.cs
+++ Seminar06_DZ_41/Program.cs
@@ -3,3 +3,37 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
+int[] EnterFigures(int length1)
+{
+    int i = 0;
+    int[] fig = new int[length1];
+    for (i = 0; i < length1; i++)
+    {
+        Console.Write("Введите число> ");
+        fig[i] = Int32.Parse(Console.ReadLine()!);
+    }
+    return fig;
+}
+
+int QuantityPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.Write("Задайте количество чисел М> ");
+int length = int.Parse(Console.ReadLine()!);
+int[] mass = EnterFigures(length);
+
+Console.WriteLine(string.Join(" ", mass));
+Console.WriteLine($"Количество положительных чисел > {QuantityPositive(mass)}");
+
+
+
