5382e4698bd6e69f67b4fe78ce88c8a735c99721
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-05-13 23:23:41 +0500

    Добавлено решение Seminar_07_DZ_52

---
+++ Seminar_07_DZ_52/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] GetArray(int rows, int cols, int minValue = 0, int maxValue = 100)
+{
+    int[,] array = new int[rows, cols];
+    var rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+void ArithmeticMeanCols(int[,] array)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        double sum = 0;
+        double sumAr = 0;
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            sum += array[i, j];
+        }
+        sumAr = sum / array.GetLength(0);
+        int n = j + 1;
+        Console.WriteLine($"Среднее арифметическое {n}-го столбца: {sumAr:F2}; ");
+    }
+}
+
+int[,] array = GetArray(2, 4);
+PrintArray(array);
+ArithmeticMeanCols(array);
