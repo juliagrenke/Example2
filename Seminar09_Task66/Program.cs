06064710cea187e813dbe44311b424972e1045df
juliagrenke <129645624+juliagrenke@users.noreply.github.com>
2023-05-29 15:09:20 +0500

    Added Seminar09_Task66

---
+++ Seminar09_Task66/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30 */
+
+
+int SumNumberOfDiapasone(int m, int n)
+{
+    if (n == m) return n;
+    int sum = m;
+    {
+        sum = m + SumNumberOfDiapasone(m + 1, n);
+    }
+    return sum;
+}
+
+Console.Write("Задайте m: ");
+int m = Int32.Parse(Console.ReadLine()!);
+Console.Write("Задайте n: ");
+int n = Int32.Parse(Console.ReadLine()!);
+Console.WriteLine($"{SumNumberOfDiapasone(m, n)}");
